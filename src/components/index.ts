7ae7d55bf217a3da11a3cb69391f94aeb70cdc88
Josmarlop16 <josmarlop16@alum.us.es>
2022-11-22 13:17:14 +0100

    fix:router

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,5 @@
 export { default as VotingCard } from "./votingCard";
 export { default as VotingList } from "./votingList";
-export { default as Login } from "./Login";
-export { default as Register } from "./Register";
 export { default as NavBar } from "./navbar";
+export { default as Login } from "./login";
+export { default as Register } from "./register";
