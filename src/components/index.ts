3fb69259af240247e72b363162b412fe342446ee
Josmarlop16 <josmarlop16@alum.us.es>
2022-11-22 13:22:13 +0100

    fix:cambio de nombre

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,5 @@
 export { default as VotingCard } from "./votingCard";
 export { default as VotingList } from "./votingList";
 export { default as NavBar } from "./navbar";
-export { default as Login } from "./login";
-export { default as Register } from "./register";
+export { default as Login } from "./signin";
+export { default as Register } from "./signup";
