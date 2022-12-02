40bccb23baa388762850bb93e1fb691fceb1baef
jvegax <toledo.utrera@hotmail.com>
2022-12-02 09:54:18 +0100

    fix: add types for voting

--- src/components/votingList/index.tsx
+++ src/components/votingList/index.tsx
@@ -2,20 +2,21 @@ import { useState } from "react";
 import useVotaciones from "../../hooks/useVotaciones";
 import VotingCard from "../votingCard";
 import { ListContainer } from "./styles";
-import {useTranslation} from "react-i18next";
+import { useTranslation } from "react-i18next";
 
 const VotingList = () => {
-    const { t } = useTranslation();
-  const { votaciones } = useVotaciones();
-  const [loading, setLoading] = useState(true);
+  const { t } = useTranslation();
+  const { votaciones, loading } = useVotaciones();
 
   const handleRenderVotaciones = () =>
-    votaciones.map((votacion) => <VotingCard key={votacion.id}  votacion={votacion} />);
+    votaciones.map((votacion) => (
+      <VotingCard key={votacion.id} votacion={votacion} />
+    ));
 
   return (
     <>
-      <h1>{t('voting_lists')}</h1>
-      <ListContainer>{loading && handleRenderVotaciones()}</ListContainer>
+      <h1>{t("voting_lists")}</h1>
+      <ListContainer>{!loading && handleRenderVotaciones()}</ListContainer>
     </>
   );
 };
